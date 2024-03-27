e24dd250f3f143ed148760a0da95af53e7b76ab5
Izyan <izzyk000@gmail.com>
2024-03-27 14:32:50 +0800

    nologin

--- src/hashed_password.py
+++
@@ -1,9 +0,0 @@
-from hasher import Hasher
-
-# Assuming you want to hash a list of passwords
-passwords_to_hash = ["myPlainPassword123", "anotherSecret!"]
-hasher = Hasher(passwords_to_hash)
-hashed_passwords = hasher.generate()
-
-# Now, hashed_passwords contains the bcrypt-hashed versions of your plain text passwords
-print(hashed_passwords)
